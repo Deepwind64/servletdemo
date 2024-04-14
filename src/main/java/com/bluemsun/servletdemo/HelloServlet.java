98208d57f8e473e218e379d410c03e03adf7682e
deepwind <deepwind32@163.com>
2024-04-14 09:44:42 +0800

    1

--- src/main/java/com/bluemsun/servletdemo/HelloServlet.java
+++ src/main/java/com/bluemsun/servletdemo/HelloServlet.java
@@ -2,21 +2,26 @@ package com.bluemsun.servletdemo;
 
 import java.io.*;
 
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
+import jakarta.servlet.ServletResponse;
 
-@WebServlet(name = "helloServlet", value = "/hello-servlet")
+
+//@WebServlet(name = "helloServlet", value = "/hello-servlet")
 public class HelloServlet extends HttpServlet {
     private String message;
+    private ServletConfig config;
 
     @Override
-    public void init() {
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
         message = "Hello World!";
     }
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("text/html");
-
         // Hello
         PrintWriter out = response.getWriter();
         out.println("<html><body>");
